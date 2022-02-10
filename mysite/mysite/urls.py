5e84b08ad1d239adb0ac8155a5317cfa313deed3
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2022-02-10 20:33:30 +0530

    added cats app to the project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',include('hello.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  
-    path('autos/', include('autos.urls')),                   
+    path('autos/', include('autos.urls')),   
+    path('cats/', include('cats.urls')),                
 ]
